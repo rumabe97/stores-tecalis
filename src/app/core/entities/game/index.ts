4978d794d0fbe7d2c1e7c50536eaea1465f22217
rumabe97 <rumabe97@gmail.com>
2025-02-02 17:34:11 +0100

    Add classes to index

--- src/app/core/entities/game/index.ts
+++ src/app/core/entities/game/index.ts
@@ -5,3 +5,35 @@ export type {GameProps} from './Game';
 export {AddedByStatus} from './AddedByStatus';
 
 export type {AddedByStatusProps} from './AddedByStatus';
+
+export {EsrbRating} from './EsrbRating';
+
+export type {EsrbRatingProps} from './EsrbRating';
+
+export {GamePlatform} from './GamePlatform';
+
+export type {GamePlatformProps} from './GamePlatform';
+
+export {GameStore} from './GameStore';
+
+export type {GameStoreProps} from './GameStore';
+
+export {Genre} from './Genre';
+
+export type {GenreProps} from './Genre';
+
+export {ParentPlatform} from './ParentPlatform';
+
+export type {ParentPlatformProps} from './ParentPlatform';
+
+export {Rating} from './Rating';
+
+export type {RatingProps} from './Rating';
+
+export {Screenshot} from './Screenshot';
+
+export type {ScreenshotProps} from './Screenshot';
+
+export {Tag} from './Tag';
+
+export type {TagProps} from './Tag';
